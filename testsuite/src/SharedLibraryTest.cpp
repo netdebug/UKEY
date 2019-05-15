db84c8bd2172ec1b0ccbfdfa63d4c3e8a49627c2
Kevin Yu <yuk@fjreach.com>
2019-05-15 20:25:24 +0800

    UKEY测试工程样本 用于所有UKEY的签章接口自动化测试

---
+++ testsuite/src/SharedLibraryTest.cpp
@@ -0,0 +1,113 @@
+//
+// SharedLibraryTest.cpp
+//
+// Copyright (c) 2004-2006, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
+
+
+#include "SharedLibraryTest.h"
+#include "CppUnit/TestCaller.h"
+#include "CppUnit/TestSuite.h"
+#include "Poco/SharedLibrary.h"
+#include "Poco/Exception.h"
+
+
+using Poco::SharedLibrary;
+using Poco::NotFoundException;
+using Poco::LibraryLoadException;
+using Poco::LibraryAlreadyLoadedException;
+
+//接口说明：
+//原型：int IsUKIn()
+//描述：判断签章设备是否连接
+//返回值：0 签章设备存在 其他值签章设备不存在
+typedef int (*IsUKIn)();
+
+//描述：获取签章数目
+//返回值：签章数目
+typedef int (*GetSealCount)();
+
+//描述：获取签章数据
+//参数：nIndex 签章索引 0为第一个章，以此类推 - 1获取所有签章
+//返回值：签章数据
+typedef std::string (*ReadSealData)(int nIndex);
+//ReadSealData 返回值格式如下：
+//<sealinfos>
+//	<sealbaseinfo>
+//		<username>用户名称</username>
+//		<projectname>项目名称</projectname>
+//	</sealbaseinfo>
+//	<sealinfo>	//如果有多个签章，则会有多个此节点
+//		<sealname>签章名称</sealname>
+//		<sealtime>签章到期时间</sealtime>
+//		<sealdata>签章印模base64数据</sealdata>
+//	</sealinfo>
+//</sealinfos>
+typedef int (*GimmeFiveFunc)();
+
+
+SharedLibraryTest::SharedLibraryTest(const std::string& name): CppUnit::TestCase(name)
+{
+}
+
+
+SharedLibraryTest::~SharedLibraryTest()
+{
+}
+
+#include <stdio.h>
+#include <sstream>
+#include "Poco/StreamCopier.h"
+
+using namespace Poco;
+
+void SharedLibraryTest::testXSSealProviderLib()
+{
+	std::string path = "XSSealProviderLib";
+	//XSSealProviderLibd.dll
+	path.append(SharedLibrary::suffix());
+	SharedLibrary sl;
+	sl.load(path);
+	//assert(sl.getPath() == path);
+	assert(sl.isLoaded());
+
+	IsUKIn isUKIn = (IsUKIn)sl.getSymbol("IsUKIn");
+	int ret = isUKIn();
+	assert(ret == 0);
+
+	GetSealCount getSealCount = (GetSealCount)sl.getSymbol("GetSealCount");
+	int count = getSealCount();
+	assert(getSealCount() > 0);
+
+	ReadSealData readSealData = (ReadSealData)sl.getSymbol("ReadSealData");
+
+	std::string data = readSealData(0);
+	std::istringstream istr(data);
+	std::ostringstream ostr;
+	std::streamsize n = StreamCopier::copyStreamUnbuffered(istr, ostr);
+
+	sl.unload();
+	assert(!sl.isLoaded());
+}
+
+void SharedLibraryTest::setUp()
+{
+}
+
+
+void SharedLibraryTest::tearDown()
+{
+}
+
+
+CppUnit::Test* SharedLibraryTest::suite()
+{
+	CppUnit::TestSuite* pSuite = new CppUnit::TestSuite("SharedLibraryTest");
+
+	CppUnit_addTest(pSuite, SharedLibraryTest, testXSSealProviderLib);
+
+	return pSuite;
+}
---
+++ testsuite/src/SharedLibraryTestSuite.cpp
@@ -0,0 +1,24 @@
+//
+// SharedLibraryTestSuite.cpp
+//
+// Copyright (c) 2004-2006, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
+
+
+#include "SharedLibraryTestSuite.h"
+#include "SharedLibraryTest.h"
+
+
+CppUnit::Test* SharedLibraryTestSuite::suite()
+{
+	CppUnit::TestSuite* pSuite = new CppUnit::TestSuite("SharedLibraryTestSuite");
+
+#if !defined(_WIN32) || defined(_DLL)
+	pSuite->addTest(SharedLibraryTest::suite());
+#endif
+
+	return pSuite;
+}
