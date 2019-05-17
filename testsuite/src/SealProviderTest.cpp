35acaf96d74558846ceece67f77a7e34e119ca67
Kevin Yu <yuk@fjreach.com>
2019-05-17 17:43:22 +0800

    更新函数名处理方式 Update Function Name Handle

--- testsuite/src/SealProviderTest.cpp
+++ testsuite/src/SealProviderTest.cpp
@@ -13,22 +13,13 @@
 #include "CppUnit/TestSuite.h"
 #include "Poco/SharedLibrary.h"
 #include "Poco/Exception.h"
-
+#include <stdio.h>
+#include <sstream>
 
 using Poco::SharedLibrary;
 using Poco::NotFoundException;
 using Poco::LibraryLoadException;
 using Poco::LibraryAlreadyLoadedException;
-
-#define CONNECT 0
-#define ALL_SEAL -1
-#define SEAL_BEGIN 0
-
-#include <stdio.h>
-#include <sstream>
-#include "Poco/StreamCopier.h"
-
-using namespace Poco;
 
 SealProviderTest::SealProviderTest(const std::string& name): CppUnit::TestCase(name)
 {
@@ -46,13 +37,15 @@ void SealProviderTest::testIsUSBKeyExist()
 	//描述：判断签章设备是否连接
 	//返回值：0 签章设备存在 其他值签章设备不存在
 	typedef int(*IsUKIn)();
+	std::string fnName("IsUKIn");
 
 	assert(sl.isLoaded());
-	assert(sl.hasSymbol("IsUKIn"));
-	IsUKIn fnTest = (IsUKIn)sl.getSymbol("IsUKIn");
+	assert(sl.hasSymbol(fnName));
+	IsUKIn fnTest = (IsUKIn)sl.getSymbol(fnName);
 	assertNotNullPtr(fnTest);
 	int val = fnTest();
 	std::ostringstream ostr;
+	ostr << "API:"<< fnName << std::endl;
 	ostr << "The function actual result is :" << val << std::endl;
 	ostr << "We will test Now!" << std::endl;
 	std::cout << ostr.str() << std::endl;
@@ -64,13 +57,15 @@ void SealProviderTest::testGetSealCount()
 	//描述：获取签章数目
 	//返回值：签章数目
 	typedef int(*GetSealCount)();
+	std::string fnName("GetSealCount");
 
 	assert(sl.isLoaded());
-	assert(sl.hasSymbol("GetSealCount"));
-	GetSealCount fnTest = (GetSealCount)sl.getSymbol("GetSealCount");
+	assert(sl.hasSymbol(fnName));
+	GetSealCount fnTest = (GetSealCount)sl.getSymbol(fnName);
 	assertNotNullPtr(fnTest);
 	int val = fnTest();
 	std::ostringstream ostr;
+	ostr << "API:" << fnName << std::endl;
 	ostr << "The function actual result is :" << val << std::endl;
 	ostr << "We will test Now!" << std::endl;
 	std::cout << ostr.str() << std::endl;
@@ -82,7 +77,6 @@ void SealProviderTest::testReadSealContent()
 	//描述：获取签章数据
 	//参数：nIndex 签章索引 0为第一个章，以此类推 - 1获取所有签章
 	//返回值：签章数据
-	typedef char* (*ReadSealData)(int nIndex);
 	//ReadSealData 返回值格式如下：
 	//<sealinfos>
 	//	<sealbaseinfo>
@@ -95,13 +89,17 @@ void SealProviderTest::testReadSealContent()
 	//		<sealdata>签章印模base64数据</sealdata>
 	//	</sealinfo>
 	//</sealinfos>
+	typedef char* (*ReadSealData)(int nIndex);
+	std::string fnName("ReadSealData");
+
 
 	assert(sl.isLoaded());
-	assert(sl.hasSymbol("ReadSealData"));
-	ReadSealData fnTest = (ReadSealData)sl.getSymbol("ReadSealData");
+	assert(sl.hasSymbol(fnName));
+	ReadSealData fnTest = (ReadSealData)sl.getSymbol(fnName);
 	assertNotNullPtr(fnTest);
-	std::string content = fnTest(all_seal);
+	std::string content = fnTest(SealProviderTest::all_seal);
 	assert(!content.empty());
+	std::cout << "API:"<< fnName << std::endl;
 	std::ostringstream ostr("We will test Now!");
 	std::cout << ostr.str() << std::endl 
 		<< content << std::endl;
