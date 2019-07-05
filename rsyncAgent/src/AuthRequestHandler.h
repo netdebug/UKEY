7287947e4aa81fa25916ca12f17d8d3bb5981da2
Kevin <anyboo@qq.com>
2019-07-05 19:59:53 +0800

    RESTful 接口的示范代码

---
+++ rsyncAgent/src/AuthRequestHandler.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+
+namespace Reach {
+
+	using Poco::Net::HTTPRequestHandler;
+	using Poco::Net::HTTPServerRequest;
+	using Poco::Net::HTTPServerResponse;
+
+	class AuthRequestHandler : public HTTPRequestHandler
+	{
+	public:
+		void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
+		{
+			response.requireAuthentication("/auth");
+			response.send();
+		}
+	};
+
+}
