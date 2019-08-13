7265a48f84ad7404b8942610c4dfbe121f438bb6
Kevin <anyboo@qq.com>
2019-08-13 18:12:35 +0800

    新增插入设备后台获取KeySN业务逻辑

--- rsyncClient/src/SampleTask.cpp
+++ rsyncClient/src/SampleTask.cpp
@@ -8,6 +8,7 @@
 #include "Poco/String.h"
 #include <cassert>
 #include <sstream>
+#include "UKeyDevice.h"
 
 using Poco::Net::HTTPRequest;
 using Poco::Net::HTTPResponse;
@@ -32,16 +33,88 @@ void SampleTask::runTask()
 	{
 		app.logger().trace("busy doing nothing... " + DateTimeFormatter::format(app.uptime()));
 
-		HTTPRequest request(HTTPRequest::HTTP_GET, "/buffer");
-		session.sendRequest(request);
-
-		HTTPResponse response;
-		std::istream& rs = session.receiveResponse(response);
-
-		app.logger().trace(format("response contentType : %s ,length : %Ld", response.getContentType(), response.getContentLength()));
-		std::ostringstream ostr;
-		StreamCopier::copyStream(rs, ostr);
-
-		app.logger().trace(format("response content : %s\n", ostr.str()));
+		if (IsUSBKeyPresent()) {
+			//addin.SetPublicParam("VirtualKeyNumber", keysn, "");
+			getUserList();
+			getKeySN();
+			sendKeySNByActiveX();
+		}
 	}
+}
+
+#include "Poco/JSON/Parser.h"
+#include "Poco/JSON/Object.h"
+#include "Poco/JSON/Array.h"
+#include "Poco/Dynamic/Var.h"
+#include "Poco/DynamicStruct.h"
+#include "Poco/Logger.h"
+
+using Poco::JSON::Parser;
+using Poco::JSON::Object;
+using Poco::JSON::Array;
+using Poco::DynamicStruct;
+using Poco::Dynamic::Var;
+
+void SampleTask::getUserList()
+{
+	Application& app = Application::instance();
+
+	HTTPResponse response;
+	HTTPRequest request(HTTPRequest::HTTP_GET, "/RS_GetUserList");
+
+	session.sendRequest(request);
+	std::istream& receive = session.receiveResponse(response);
+
+	Parser json;
+	Var result = json.parse(receive);
+	DynamicStruct ds = *result.extract<Object::Ptr>();
+	_userlist = ds["data"]["userlist"].toString();
+
+	_ukey = new UKeyDevice(_userlist);
+
+	poco_trace_f2(app.logger(), "getUserList < %s : %s >\n", _ukey->containerName(), _ukey->certificateName());
+}
+
+void SampleTask::getKeySN()
+{
+	Application& app = Application::instance();
+
+	HTTPResponse response;
+	HTTPRequest request(HTTPRequest::HTTP_GET, "/RS_KeyGetKeySn");
+	std::string body(format("containerId=%s", _ukey->containerName()));
+	request.setContentLength((int)body.length());
+	session.sendRequest(request) << body;
+
+	session.sendRequest(request);
+	std::istream& receive = session.receiveResponse(response);
+
+	Parser json;
+	Var result = json.parse(receive);
+	DynamicStruct ds = *result.extract<Object::Ptr>();
+
+	poco_trace_f1(app.logger(), "getKeySN : %s\n" , ds["data"]["keySn"].toString());
+}
+
+void SampleTask::sendKeySNByActiveX()
+{
+	Application& app = Application::instance();
+	poco_trace(app.logger(), "sendKeySNByActiveX : \n");
+}
+
+bool SampleTask::IsUSBKeyPresent()
+{
+	Application& app = Application::instance();
+
+	HTTPResponse response;
+	HTTPRequest request(HTTPRequest::HTTP_GET, "/buffer");
+
+	session.sendRequest(request);
+	std::istream& receive = session.receiveResponse(response);
+
+	Parser json;
+	Var result = json.parse(receive);
+	Array da = *result.extract<Array::Ptr>();
+	poco_trace_f1(app.logger(), "IsUSBKeyPresent response content : %s\n", result.toString());
+
+	return da.size() > 0;
 }
--- rsyncClient/src/SampleTask.h
+++ rsyncClient/src/SampleTask.h
@@ -1,20 +1,31 @@
 #pragma once
 #include "Poco/Task.h"
 #include "Poco/Net/HTTPClientSession.h"
+#include "Poco/SharedPtr.h"
+#include <string>
 
 namespace Reach {
 
 	using Poco::Task;
 	using Poco::Net::HTTPClientSession;
 
+	class UKeyDevice;
 	class SampleTask : public Task
 	{
 	public:
 		SampleTask(std::string& host, short port);
 
 		void runTask();
+
+	private:
+		void getUserList();
+		void getKeySN();
+		void sendKeySNByActiveX();
+		bool IsUSBKeyPresent();
 		
 	private:
 		HTTPClientSession session;
+		std::string _userlist;
+		Poco::SharedPtr<UKeyDevice> _ukey;
 	};
 }
