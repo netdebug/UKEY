73fb5a9a7ec70c98f2e5faf71fbe56bba4b45413
Kevin <anyboo@qq.com>
2020-01-10 16:20:40 +0800

    飞天设备支持 Notes： 1.RSA,SM2证书支持 2.FJCA_FCardCert.dll动态库

--- rsyncClient/src/FT3000GMMedia.cpp
+++ rsyncClient/src/FT3000GMMedia.cpp
@@ -9,14 +9,21 @@ using namespace Reach::ActiveX;
 using Poco::Base64Encoder;
 using Poco::Base64EncodingOptions;
 
+extern std::string SOF_GetCertInfoByOid(std::string Base64EncodeCert, std::string oid);
+
 FT3000GMMedia::FT3000GMMedia()
+	:_hDev(0)
 {
 	Utility::message("Enter FT3000GMMedia");
 	setProperty("Provider", "FT3000GMMedia");
+	sl.load("FJCA_FCardCert.dll");
+	open();
 }
 
 FT3000GMMedia::~FT3000GMMedia()
 {
+	close();
+	sl.unload();
 	Utility::message("Exit FT3000GMMedia");
 }
 
@@ -30,30 +37,128 @@ void FT3000GMMedia::extract()
 
 void FT3000GMMedia::GetCertBase64String()
 {
-	int rv = 0;
-	HANDLE hDev = NULL;
-	std::string content;
+	if (hasCert("RSA"))
+		setProperty("cert", readRSACert());
+	else if (hasCert("ECC"))
+		setProperty("cert", readECCCert());
+	else
+		poco_assert(0);
+}
 
-	rv = InitFCard("USB1", &hDev);
+void FT3000GMMedia::open()
+{
+	typedef BOOL (*InitFCard)(
+		IN char			*szDevName,
+		OUT DEVHANDLE	*phDev
+	);
+
+	InitFCard pfn = (InitFCard)sl.getSymbol("InitFCard");
+	poco_assert(pfn);
+
+	int rv = 0;
+	rv = (*pfn)("USB1", &_hDev);
 	poco_assert(rv);
+}
+
+void FT3000GMMedia::close()
+{
+	typedef BOOL (*ExitFCard)(
+		IN DEVHANDLE	hDev
+	);
+
+	ExitFCard pfn = (ExitFCard)sl.getSymbol("ExitFCard");
+	poco_assert(pfn);
+
+	int rv = 0;
+	rv = (*pfn)(_hDev);
+	poco_assert(rv);
+}
+
+bool FT3000GMMedia::hasCert(const std::string& type)
+{
+	if (type == "RSA")
+		return hasRSACert();
+	else if (type == "ECC")
+		return hasECCCert();
+	else
+		poco_assert(0);
+
+	return false;
+}
+
+bool FT3000GMMedia::hasRSACert()
+{
+	typedef BOOL (*FCardGetCertNo)(
+		OUT BYTE		*pbCertNo,
+		IN DEVHANDLE	hDev
+	);
+
+	FCardGetCertNo pfn = (FCardGetCertNo)sl.getSymbol("FCardGetCertNo");
+	poco_assert(pfn);
 
 	BYTE no = 0;
-	rv = FCardGetCertNo(&no, hDev);
-	poco_assert(rv);
+	int rv = 0;
 
-	rv = FCardSetCertNo(0x01, hDev);
+	rv = (*pfn)(&no, _hDev);
+	poco_assert(rv);
+	if (no > 0) 
+		return true;
+	return false;
+}
+
+bool FT3000GMMedia::hasECCCert()
+{
+	typedef BOOL (*FCardECCGetCertNo)(
+		OUT BYTE		*pbCertNo,
+		IN DEVHANDLE	hDev
+	);
+
+	FCardECCGetCertNo pfn = (FCardECCGetCertNo)sl.getSymbol("FCardECCGetCertNo");
+	poco_assert(pfn);
+
+	BYTE no = 0;
+	int rv = 0;
+
+	rv = (*pfn)(&no, _hDev);
+	poco_assert(rv);
+	if (no > 0)
+		return true;
+	return false;
+}
+
+std::string FT3000GMMedia::readRSACert()
+{
+	typedef BOOL (*FCardSetCertNo)(
+		IN BYTE			byCertNo,
+		IN DEVHANDLE	hDev
+	);
+
+	FCardSetCertNo set = (FCardSetCertNo)sl.getSymbol("FCardSetCertNo");
+	poco_assert(set);
+
+	typedef BOOL (*FCardReadCert)(
+		OUT BYTE		*pbCert,
+		IN OUT DWORD	*pdwCertLen,
+		IN DEVHANDLE	hDev
+	);
+
+	FCardReadCert read = (FCardReadCert)sl.getSymbol("FCardReadCert");
+	poco_assert(read);
+
+	int rv = 0;
+
+	rv = (*set)(0x01, _hDev);
 	poco_assert(rv);
 
 	DWORD len = 2048;
 	std::vector<char> vCert(len, 0);
-	rv = FCardReadCert((BYTE*)vCert.data(), &len, hDev);
+	rv = (*read)((BYTE*)vCert.data(), &len, _hDev);
 	if (!rv && len > vCert.size()) {
 		vCert.resize(len + 1);
-		rv = FCardReadCert((BYTE*)vCert.data(), &len, hDev);
+		rv = (*read)((BYTE*)vCert.data(), &len, _hDev);
 	}
 
 	poco_assert(rv);
-
 	vCert.resize(len);
 
 	std::ostringstream ostr;
@@ -61,12 +166,52 @@ void FT3000GMMedia::GetCertBase64String()
 	encoder.write(vCert.data(), vCert.size());
 	encoder.close();
 
-	setProperty("cert", ostr.str());
-	rv = ExitFCard(hDev);
+	return ostr.str();
+}
+
+std::string FT3000GMMedia::readECCCert()
+{
+	typedef BOOL (*FCardECCSetCertNo)(
+		IN BYTE			byCertNo,
+		IN DEVHANDLE	hDev
+	);
+	FCardECCSetCertNo eccSet = (FCardECCSetCertNo)sl.getSymbol("FCardECCSetCertNo");
+	poco_assert(eccSet);
+
+	typedef BOOL (*FCardECCReadCert)(
+		OUT BYTE		*pbCert,
+		IN OUT DWORD	*pdwCertLen,
+		IN DEVHANDLE	hDev
+	);
+	FCardECCReadCert eccRead = (FCardECCReadCert)sl.getSymbol("FCardECCReadCert");
+
+	int rv = 0;
+
+	rv = (*eccSet)(0x01, _hDev);
 	poco_assert(rv);
+
+	DWORD len = 2048;
+	std::vector<char> vCert(len, 0);
+	rv = (*eccRead)((BYTE*)vCert.data(), &len, _hDev);
+	if (!rv && len > vCert.size()) {
+		vCert.resize(len + 1);
+		rv = (*eccRead)((BYTE*)vCert.data(), &len, _hDev);
+	}
+
+	poco_assert(rv);
+	vCert.resize(len);
+
+	std::ostringstream ostr;
+	Poco::Base64Encoder encoder(ostr);
+	encoder.write(vCert.data(), vCert.size());
+	encoder.close();
+
+	return ostr.str();
 }
 
 void FT3000GMMedia::FetchKeySN()
 {
-
+	std::string cert = getProperty("cert");
+	std::string text = SOF_GetCertInfoByOid(cert, "1.2.156.112578.1");
+	utility_message(text);
 }
--- rsyncClient/src/FT3000GMMedia.h
+++ rsyncClient/src/FT3000GMMedia.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "MediaBase.h"
+#include <string>
+#include "Poco/SharedLibrary.h"
 
 namespace Reach {
 
@@ -9,11 +11,23 @@ namespace Reach {
 	public:
 		FT3000GMMedia();
 		~FT3000GMMedia();
-
+		void open();
+		void close();
+		bool hasCert(const std::string & type);
 		virtual void extract();
 
 	protected:
 		virtual void GetCertBase64String();
+		
+		bool hasRSACert();
+		bool hasECCCert();
+		std::string readRSACert();
+		std::string readECCCert();
 		virtual void FetchKeySN();
+		
+	private:
+		Poco::SharedLibrary sl;
+		typedef void* HANDLE;
+		HANDLE _hDev;
 	};
 }
